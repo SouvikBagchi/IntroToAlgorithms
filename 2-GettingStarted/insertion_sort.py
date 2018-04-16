bd355f9becb13b30972d0bf1b8a43dd5dec401c9
souvikbagchi <souvik.bagchi@gmail.com>
2018-04-16 22:07:13 +0530

    insertion sort implemented

--- 2-GettingStarted/insertion_sort.py
+++ 2-GettingStarted/insertion_sort.py
@@ -7,11 +7,49 @@ class InsertionSort():
 #This method sorts the array by the insertion sort technique and prints the sorted array as well as returns it
 #This method also returns the time it takes to sort the array
  def sort(self):
+  print("Original array : "+str(self.array))
+  sorted_array = self.array
   current_time = datetime.datetime.now()
-  sorted_array = [2,3,4]
+  for i in range(1,len(sorted_array)-1):
+   # print("array is ")
+   # print(sorted_array)
+   # print("i is ")
+   # print(i)
+   # print("ith element is ")
+   # print(sorted_array[i])
+   key = sorted_array[i]
+   for j in range(0,i):
+    # print("j is ")
+    # print(j)
+    # print('jth element is ')
+    # print(sorted_array[j])
+    
+
+    if sorted_array[j] > key:
+     #Store the jth element in a temporary space
+     temp = sorted_array[j]
+     # print('key is')
+     # print(key)
+     del sorted_array[i]
+     sorted_array.insert(0,key)
+     #Change the key after the swap
+     key = temp
+
+     # print("changed array ")
+     # print(sorted_array)
+     # print('')
+
+    else:
+     # print("breaking")
+     # print('')
+     break
+
+
   finished_time = datetime.datetime.now()
   
-  print("The sorted numbers are : " + str(self.printArray(sorted_array)))
+  print("Sorted array : "+ str(sorted_array))
+  print("The time taken : "+str(finished_time-current_time))
+  return sorted_array
 
 
  def printArray(self,array):
@@ -19,4 +57,5 @@ class InsertionSort():
    print(element)
 
 y = InsertionSort([4,5,6,2,7])
-y.sort()
+final_array = y.sort()
+print(final_array)
